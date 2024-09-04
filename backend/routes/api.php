1deb9ab122d6f9b5521577e036f3daae704df28c
Fēlikss <felikss.regzdins@gmail.com>
2024-09-04 21:44:39 +0300

    moved API route to api.php from web.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -13,3 +13,7 @@ Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('logout', [AuthController::class, 'logout']);
 Route::middleware('auth:sanctum')->put('user', [AuthController::class, 'update']);
+
+use App\Http\Controllers\WeatherController;
+
+Route::get('/weather', [WeatherController::class, 'getWeather']);
--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\WeatherController;
-
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/api/weather', [WeatherController::class, 'getWeather']);
