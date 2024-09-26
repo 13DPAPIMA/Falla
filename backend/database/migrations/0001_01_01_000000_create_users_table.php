14d8cdc2e6661b9e197d396294864d91cdbc39b7
23DPAPIMA <103630425+13DPAPIMA@users.noreply.github.com>
2024-09-26 20:07:04 +0300

    Unsigned default value for gender column

--- backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('role')->default('user');
-            $table->enum('gender', ['Male', 'Female'])->default('Undefined');
+            $table->enum('gender', ['Male', 'Female']);
             $table->rememberToken();
             $table->timestamps();
         });
