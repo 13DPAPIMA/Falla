0ea391c086808906e987ca7bbd5883be7c8e014d
Fēlikss <felikss.regzdins@gmail.com>
2024-10-17 09:24:10 +0300

    added routes for wardrobe logic

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,23 +2,48 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\WeatherController;
-use App\Http\Controllers\ClothingSuggestionsController;
-use App\Http\Controllers\PhotoController;
+
+
+// user routes
+use App\Http\Controllers\AuthController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
-
-use App\Http\Controllers\AuthController;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 Route::middleware('auth:sanctum')->post('logout', [AuthController::class, 'logout']);
 Route::middleware('auth:sanctum')->put('user', [AuthController::class, 'update']);
 
+
+// weather and suggestion routes
+use App\Http\Controllers\WeatherController;
+use App\Http\Controllers\ClothingSuggestionsController;
+
 Route::get('/weather', [WeatherController::class, 'getWeather']);
 
 Route::post('/clothing-suggestions', [ClothingSuggestionsController::class, 'getClothingSuggestions']);
 
 Route::post('/upload-photo', [App\Http\Controllers\PhotoController::class, 'uploadPhoto']);
+
+
+// warbrobe routes
+use App\Http\Controllers\ClothingController;
+use App\Http\Controllers\WardrobeController;
+use App\Http\Controllers\WardrobeItemController;
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/wardrobe', [WardrobeController::class, 'index']);
+    Route::get('/wardrobe/available-clothing', [WardrobeController::class, 'availableClothing']);
+    Route::post('/wardrobe', [WardrobeController::class, 'addToWardrobe']);
+    Route::delete('/wardrobe/{id}', [WardrobeController::class, 'removeFromWardrobe']);
+
+    Route::post('/wardrobe-items', [WardrobeItemController::class, 'addToWardrobe']);
+    Route::put('/wardrobe-items/{id}', [WardrobeItemController::class, 'updateWardrobeItem']);
+    Route::delete('/wardrobe-items/{id}', [WardrobeItemController::class, 'deleteWardrobeItem']);
+
+    Route::get('/clothing', [ClothingController::class, 'index']);
+    Route::get('/clothing/{id}', [ClothingController::class, 'show']);
+});
