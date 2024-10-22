e0a22544a612922bf947e202ead1191a7f4447ab
ATarv <13dpatarvida@rvt.lv>
2024-10-22 16:33:13 +0300

    Added controller, didn't test it, should do 'php artisan migrate

---
+++ backend/app/Http/Controllers/Api/QuestionController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Question;
+use Illuminate\Http\Request;
+
+class QuestionController extends Controller
+{
+    // Create
+    public function store(Request $request)
+    {
+        $request->validate([
+            'topic' => 'required|in:Suggestions,Clothes,Other',
+            'photo_link' => 'nullable|url',
+            'description' => 'required|string',
+        ]);
+
+        $question = Question::create($request->all());
+        return response()->json($question, 201);
+    }
+
+    // Read (all)
+    public function index()
+    {
+        $questions = Question::all();
+        return response()->json($questions);
+    }
+
+    // Read (single)
+    public function show($id)
+    {
+        $question = Question::findOrFail($id);
+        return response()->json($question);
+    }
+
+    // Update
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'topic' => 'sometimes|required|in:Suggestions,Clothes,Other',
+            'photo_link' => 'nullable|url',
+            'description' => 'sometimes|required|string',
+        ]);
+
+        $question = Question::findOrFail($id);
+        $question->update($request->all());
+        return response()->json($question);
+    }
+
+    // Delete
+    public function destroy($id)
+    {
+        $question = Question::findOrFail($id);
+        $question->delete();
+        return response()->json(null, 204);
+    }
+}
