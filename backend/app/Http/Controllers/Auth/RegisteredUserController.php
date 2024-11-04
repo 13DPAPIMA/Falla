9908fa1b27cff6dac753e061d37f01e8153006f5
Fēlikss <felikss.regzdins@gmail.com>
2024-11-04 16:48:26 +0200

    Delete name from User model

--- backend/app/Http/Controllers/Auth/RegisteredUserController.php
+++ backend/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -21,13 +21,11 @@ class RegisteredUserController extends Controller
     public function store(Request $request): Response
     {
         $request->validate([
-            'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = User::create([
-            'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->string('password')),
         ]);
--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -18,7 +18,6 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
         'email',
         'password',
         'gender',
