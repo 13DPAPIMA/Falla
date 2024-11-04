7ae2e3dff56715a3aa4ba7185f972cbe567fa402
Fēlikss <felikss.regzdins@gmail.com>
2024-11-04 19:11:40 +0200

    Refactor auth routes to use sanctum auth

--- backend/routes/auth.php
+++ backend/routes/auth.php
@@ -25,13 +25,13 @@ Route::post('/reset-password', [NewPasswordController::class, 'store'])
     ->name('password.store');
 
 Route::get('/verify-email/{id}/{hash}', VerifyEmailController::class)
-    ->middleware(['auth', 'signed', 'throttle:6,1'])
+    ->middleware(['auth:sanctum', 'signed', 'throttle:6,1'])
     ->name('verification.verify');
 
 Route::post('/email/verification-notification', [EmailVerificationNotificationController::class, 'store'])
-    ->middleware(['auth', 'throttle:6,1'])
+    ->middleware(['auth:sanctum', 'throttle:6,1'])
     ->name('verification.send');
 
 Route::post('/logout', [AuthenticatedSessionController::class, 'destroy'])
-    ->middleware('auth')
+    ->middleware('auth:sanctum')
     ->name('logout');
