2f3c7b3e01462bdb476d639c123dcffdea9884ba
Fēlikss <felikss.regzdins@gmail.com>
2024-11-05 11:39:19 +0200

    Refactor email verification response to JSON format

--- backend/app/Http/Controllers/Auth/VerifyEmailController.php
+++ backend/app/Http/Controllers/Auth/VerifyEmailController.php
@@ -5,27 +5,22 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
-use Illuminate\Http\RedirectResponse;
 
 class VerifyEmailController extends Controller
 {
     /**
      * Mark the authenticated user's email address as verified.
      */
-    public function __invoke(EmailVerificationRequest $request): RedirectResponse
+    public function __invoke(EmailVerificationRequest $request): \Illuminate\Http\JsonResponse
     {
         if ($request->user()->hasVerifiedEmail()) {
-            return redirect()->intended(
-                config('app.frontend_url').'/dashboard?verified=1'
-            );
+            return response()->json('Already Verified');
         }
 
         if ($request->user()->markEmailAsVerified()) {
             event(new Verified($request->user()));
         }
 
-        return redirect()->intended(
-            config('app.frontend_url').'/dashboard?verified=1'
-        );
+        return response()->json('Successfully Verified');
     }
 }
